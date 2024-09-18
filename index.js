a9a5483603929b56c63b5ff9313a92965e13d99b
Nandani <nks854338@gmail.com>
2024-09-18 11:57:02 +0530

    commited

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", userRoute);
+app.use("/user", userRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
